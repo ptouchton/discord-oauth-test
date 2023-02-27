1cd19b8487ce0d77601d3c88a6c2e04bfe3bd4f7
atulpatare <atul.patare@launchventures.co>
2023-02-27 18:05:46 +0530

    added discord app to server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-const variable: string = "this is string"
-console.log("this from ts file ", variable)
+import config from "./config";
+import * as discord from "./discord";
+import express from "express";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const { code } = req.query;
+  if (code) {
+    try {
+      const tokenResp = await discord.getAccessToken(code as string);
+      const user = await discord.getProfile(
+        tokenResp.data.token_type,
+        tokenResp.data.access_token
+      );
+      console.log(user.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return res.sendFile("public/index.html", { root: "." });
+});
+
+app.listen(config.port, () => {
+  console.log(`Server listening on http://localhost:${config.port}`);
+});
